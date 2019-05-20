b97d6ccfac3514863cdb66f61a5cf44aa1b6668b
Arthur Lima <arthur-esc@hotmail.com>
2019-05-20 19:53:30 -0300

    [front] Dotenv e componente da tabela de máquinas.

---
+++ front/src/components/MachineTable/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Table } from "./styles";
+
+const MachineTable = () => (
+  <Table>
+    <thead>
+      <tr>
+        <td>id</td>
+        <td>Nome</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Máquina 1</td>
+      </tr>
+    </tbody>
+  </Table>
+);
+export default MachineTable;
---
+++ front/src/components/MachineTable/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+  margin-top: 30px;
+  border-collapse: collapse;
+  width: 100%;
+  thead {
+    background: #2c3e50;
+    td {
+      color: white;
+      font-weight: bold;
+    }
+  }
+  td {
+    padding-left: 20px;
+    height: 35px;
+  }
+`;
